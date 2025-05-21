9f70695c62bbd858e1313f869b48c8d5b60f691d
NormalHuman <111682104+YanardRognuh@users.noreply.github.com>
2025-05-21 15:12:22 +0700

    PWA Implemented for Installation and Offline

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,27 @@
-const path = require('path');
-const common = require('./webpack.common.js');
-const { merge } = require('webpack-merge');
+const path = require("path");
+const common = require("./webpack.common.js");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: {
+      directory: path.resolve(__dirname, "src"),
+      watch: true, // awasi src, bukan dist
+    },
+    devMiddleware: {
+      publicPath: "/",
+    },
     port: 9000,
+    open: false,
     client: {
       overlay: {
         errors: true,
